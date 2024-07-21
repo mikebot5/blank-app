fb0b2fb065eb82a098fd941d79415e8f283dae1b
mikebot5 <164817319+mikebot5@users.noreply.github.com>
2024-07-21 15:10:27 -0700

    Create gfu.py

---
+++ .github/gfu.py
@@ -0,0 +1,24 @@
+def get_first_url(url):
+    import efl
+    import requests
+    from bs4 import BeautifulSoup
+
+    # Get the filtered links
+    urls = efl.extract_filtered_links(url)
+
+    if urls:
+        # Retrieve content from the first link (if available)
+        response = requests.get(urls[0])
+        response2 = requests.get(urls[1])
+        soup = BeautifulSoup(response.content, 'html.parser')
+        soup2 = BeautifulSoup(response2.content, 'html.parser')
+        text_content = soup.get_text()
+        text_content2 = soup2.get_text()
+
+        # Print the actual text content
+        return(text_content + text_content2)
+    else:
+        print("No filtered links found.")
+
+if __name__ == '__main__':
+    print(get_first_url('dogs'))
